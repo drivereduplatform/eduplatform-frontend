91a8840558cca1d1ce953f76698d61e88f971f7d
sabyr-cpu <sab.sabyr@gmail.com>
2022-01-16 13:20:03 +0600

    modified login screen -added `cursor: pointer` to the eduplatform logo -changed password input type to `password` -changed indention in two files

--- src/comp/login/Main.jsx
+++ src/comp/login/Main.jsx
@@ -1,50 +1,66 @@
-import { Link } from "react-router-dom";
-import { useState } from "react";
+import { Link } from 'react-router-dom'
+import { useState } from 'react'
 import axios from 'axios'
 import api from '../../configs/api'
 
-function Main({ language, setSteps }) {
+function Main ({ language, setSteps }) {
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+  const [checkbox, setCheckbox] = useState(false)
 
-    const [username, setUsername] = useState('')
-    const [password, setPassword] = useState('')
-    const [checkbox, setCheckbox] = useState(false)
+  function loginRequest (e) {
+    e.preventDefault()
+    axios
+      .post(`${api.api_url}/login`, {
+        username: username,
+        password: password
+      })
+      .then(response => {
+        console.log(response)
+      })
+  }
 
-    function loginRequest(e) {
-        e.preventDefault()
-        axios.post(`${api.api_url}/login`, {
-            username: username,
-            password: password
-        }).then(response => {
-            console.log(response);
-        })
-    }
-
-    return (
-        <section className="login">
-            <div className="container">
-                <div className="login-inner inner">
-                    <div className="login-title title">
-                        <h1>{language.login.title}</h1>
-                    </div>
-                    <form onSubmit={(e) => loginRequest(e)} className="login-form">
-                        <div className="login-form-input">
-                            <input onChange={(e) => setUsername(e.target.value)} placeholder={language.login.inputs.username} type="text" />
-                        </div>
-                        <div className="login-form-input">
-                            <input onChange={(e) => setPassword(e.target.value)} placeholder={language.login.inputs.password} type="text" />
-                        </div>
-                        <div className="login-form-checkbox note">
-                            <input onChange={() => setCheckbox(!checkbox)} type="checkbox" /> <div className="login-form-checkbox-indicator"></div><p>{language.login.buttons.rememberMe}</p>
-                        </div>
-                        <button onClick={(e) => loginRequest(e)} className="login-form-button">{language.login.buttons.login}</button>
-                        <Link to="/register" className="login-form-note note">
-                            <p>{language.login.buttons.dontHaveAccount}</p>
-                        </Link>
-                    </form>
-                </div>
+  return (
+    <section className='login'>
+      <div className='container'>
+        <div className='login-inner inner'>
+          <div className='login-title title'>
+            <h1>{language.login.title}</h1>
+          </div>
+          <form onSubmit={e => loginRequest(e)} className='login-form'>
+            <div className='login-form-input'>
+              <input
+                onChange={e => setUsername(e.target.value)}
+                placeholder={language.login.inputs.username}
+                type='text'
+              />
             </div>
-        </section>
-    )
+            <div className='login-form-input'>
+              <input
+                onChange={e => setPassword(e.target.value)}
+                placeholder={language.login.inputs.password}
+                type='password'
+              />
+            </div>
+            <div className='login-form-checkbox note'>
+              <input onChange={() => setCheckbox(!checkbox)} type='checkbox' />{' '}
+              <div className='login-form-checkbox-indicator'></div>
+              <p>{language.login.buttons.rememberMe}</p>
+            </div>
+            <button
+              onClick={e => loginRequest(e)}
+              className='login-form-button'
+            >
+              {language.login.buttons.login}
+            </button>
+            <Link to='/register' className='login-form-note note'>
+              <p>{language.login.buttons.dontHaveAccount}</p>
+            </Link>
+          </form>
+        </div>
+      </div>
+    </section>
+  )
 }
 
-export default Main;
+export default Main
--- src/comp/login/Sidebar.jsx
+++ src/comp/login/Sidebar.jsx
@@ -1,42 +1,45 @@
-function Sidebar({ step, language }) {
+function Sidebar ({ step, language }) {
+  const stepItems = [
+    {
+      step: language.login.steps.loginToAccount
+    }
+  ]
 
-    const stepItems = [
-        {
-            step: language.login.steps.loginToAccount
-        }
-    ]
-
-    return (
-        <aside>
-            <div className="aside-inner mini-inner">
-                <div className="aside-top">
-                    <div className="aside-logo">
-                        <h1>-eduplatform-</h1>
-                    </div>
+  return (
+    <aside>
+      <div className='aside-inner mini-inner'>
+        <div className='aside-top'>
+          <div className='aside-logo'>
+            <h1>-eduplatform-</h1>
+          </div>
+        </div>
+        <div className='aside-bottom'>
+          <div className='aside-steps'>
+            {stepItems.map((stepItem, key) => (
+              <div key={key} className='aside-step'>
+                <div className='aside-step-left'>
+                  <div
+                    className={
+                      step === key + 1
+                        ? 'aside-step-circle active'
+                        : 'aside-step-circle'
+                    }
+                  >
+                    <h1>{key + 1}</h1>
+                  </div>
                 </div>
-                <div className="aside-bottom">
-                    <div className="aside-steps">
-                        {
-                            stepItems.map((stepItem, key) => (
-                                <div key={key} className="aside-step">
-                                    <div className="aside-step-left">
-                                        <div className={step === (key + 1) ? 'aside-step-circle active' : 'aside-step-circle'}>
-                                            <h1>{key + 1}</h1>
-                                        </div>
-                                    </div>
-                                    <div className="aside-step-right">
-                                        <div className="aside-step-title link">
-                                            <h1>{stepItem.step}</h1>
-                                        </div>
-                                    </div>
-                                </div>
-                            ))
-                        }
-                    </div>
+                <div className='aside-step-right'>
+                  <div className='aside-step-title link'>
+                    <h1>{stepItem.step}</h1>
+                  </div>
                 </div>
-            </div>
-        </aside>
-    )
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+    </aside>
+  )
 }
 
 export default Sidebar
