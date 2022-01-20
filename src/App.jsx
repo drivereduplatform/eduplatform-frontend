1932ec76c5ab768ce5e9d06cb1b655061913cfb2
sabyr-cpu <sab.sabyr@gmail.com>
2022-01-20 20:29:39 +0600

    Made a redirect in `/` route

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom'
 import Login from './screens/Login';
-import Main from './screens/Main';
 import Register from './screens/Register';
 import './style-global.css'
 import './style-main.css'
@@ -9,7 +8,7 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Main />} />
+        <Route path="/" element={<Navigate to="/login" />} />
         <Route path="/register" element={<Register />} />
         <Route path="/login" element={<Login />} />
       </Routes>
